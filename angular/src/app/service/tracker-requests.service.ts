30fb7a4dcbf74156c4f65e5d0eac71813a03b414
yuvaraj <yuvaa.it@gmail.com>
2024-03-10 19:21:16 +0100

    Implement all services

--- angular/src/app/service/tracker-requests.service.ts
+++ angular/src/app/service/tracker-requests.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +11,93 @@ export class TrackerRequestsService {
   constructor(private http: HttpClient) { }
 
   addTeam(team): Observable<any> {
-    return; //'POST' request , "/api/tracker/technologies/add", send team details to backend to add
+    //'POST' request , "/api/tracker/technologies/add", send team details to backend to add
+    return this.http.post("/api/tracker/technologies/add", team).pipe(
+      map((response: any) => {
+        return response;
+      }),
+      catchError((error) => {
+        return Observable.throw({
+          status: error.status,
+          statusText: 'Invalid username or password',
+          error: {
+            message: 'Invalid username or password'
+          }
+        });
+      })
+    );
   }
 
   getTeams(): Observable<any> {
-    return; //'GET' request , "/api/tracker/technologies/get", get all team names from backend     
+    //'GET' request , "/api/tracker/technologies/get", get all team names from backend     
+    return this.http.get("/api/tracker/technologies/get").pipe(
+      map((response: any) => {
+        return response;
+      }),
+      catchError((error) => {
+        return Observable.throw({
+          status: error.status,
+          statusText: 'Invalid username or password',
+          error: {
+            message: 'Invalid username or password'
+          }
+        });
+      })
+    );
   }
 
   removeTeam(team): Observable<any> {
-    return; //'DELETE' request , "/api/tracker/technologies/remove/", delete a team name in backend     
+    //'DELETE' request , "/api/tracker/technologies/remove/", delete a team name in backend     
+    return this.http.delete(`/api/tracker/technologies/remove/${team}`).pipe(
+      map((response: any) => {
+        return response;
+      }),
+      catchError((error) => {
+        return Observable.throw({
+          status: error.status,
+          statusText: 'Invalid username or password',
+          error: {
+            message: 'Invalid username or password'
+          }
+        });
+      })
+    );
   }
 
   addMember(member): Observable<any> {
-    return; //'POST' request , "/api/tracker/members/add", send the member details to backend server to add in DB     
+    //'POST' request , "/api/tracker/members/add", send the member details to backend server to add in DB     
+    return this.http.post("/api/tracker/members/add", member).pipe(
+      map((response: any) => {
+        return response;
+      }),
+      catchError((error) => {
+        return Observable.throw({
+          status: error.status,
+          statusText: 'Invalid username or password',
+          error: {
+            message: 'Invalid username or password'
+          }
+        });
+      })
+    );
   }
 
   getMembers(): Observable<any> {
-    return; //'GET' request , "/api/tracker/members/display", get all member details from backend          
+    //'GET' request , "/api/tracker/members/display", get all member details from backend       
+    return this.http.get("/api/tracker/members/display").pipe(
+      map((response: any) => {
+        return response;
+      }),
+      catchError((error) => {
+        return Observable.throw({
+          status: error.status,
+          statusText: 'Invalid username or password',
+          error: {
+            message: 'Invalid username or password'
+          }
+        });
+      })
+    );
   }
 
 }
